45029532176eb5aa2cf911d1588c3f1846b165f3
Karanvaibhav <97182151+Karanvaibhav@users.noreply.github.com>
2023-04-30 11:23:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,62 +1,56 @@
 const express = require("express");
-const connectDB = require("./config/db");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-const path = require("path");
-const cors=require("cors");
 dotenv.config();
 connectDB();
-const app = express();
+const path = require("path");
 
-app.use(express.json()); // to accept json data
+const app = express();
+app.use(express.json());
 
 // app.get("/", (req, res) => {
-//   res.send("API Running!");
+//   res.send("API is Running ");
 // });
-
-
-app.use(cors());
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-// // --------------------------deployment------------------------------
+// --------------------------deployment------------------------------
+const __dirname1 = path.resolve();
 
-// const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
+// --------------------------deployment------------------------------
 
-// // --------------------------deployment------------------------------
-
-// Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT;
-
 const server = app.listen(
   PORT,
-  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+  console.log(`server started on port ${PORT}`.yellow.bold)
 );
 
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "*",
+    origin: "http://localhost:3000",
     // credentials: true,
   },
 });
@@ -72,6 +66,7 @@ io.on("connection", (socket) => {
     socket.join(room);
     console.log("User Joined Room: " + room);
   });
+
   socket.on("typing", (room) => socket.in(room).emit("typing"));
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
